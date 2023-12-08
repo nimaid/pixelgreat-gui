bb3e83438f086b6bc1fde3c0f2935c1398dd4fee
Ella Jameson <ellagjameson@gmail.com>
2023-12-08 06:13:52 -0700

    Fixed path errors and generalized

--- pixelgreat_gui/constants/paths.py
+++ pixelgreat_gui/constants/paths.py
@@ -4,11 +4,9 @@ import sys
 # Test if this is a PyInstaller executable or a .py file
 if getattr(sys, 'frozen', False):
     IS_EXE = True
-    PROG_FILE = sys.executable
-    PROG_PATH = os.path.dirname(PROG_FILE)
-    PATH = os.path.join(sys._MEIPASS, "pixelgreat_gui")
+    PROG_PATH = os.path.dirname(sys.executable)
+    PATH = os.path.join(sys._MEIPASS, __name__.split(".")[0])
 else:
     IS_EXE = False
     PROG_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    PROG_FILE = os.path.join(PROG_PATH, "__main__.py")
     PATH = PROG_PATH
